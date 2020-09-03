814b06147362dbd68753a9039038204732333605
DJ Viking <anders.swedishviking@gmail.com>
2020-09-03 01:52:48 -0400

    upvote js

---
+++ public/javascript/upvote.js
@@ -0,0 +1,21 @@
+const upvoteClickHandler = async (event) => {
+  try {
+    event.preventDefault();
+    const windowLoc = window.location.toString().split('/');
+    const id = windowLoc[windowLoc - 1];
+    const response = await fetch('/api/posts/upvote', {
+      method: 'PUT',
+      body: JSON.stringify(
+        {
+          post_id: id
+        }
+      ),
+      headers:{'Content-Type': 'application/json'}
+    });
+    response.ok ? document.location.reload() : console.log("There was an error."); console.log(response.statusText);
+  } catch (error) {
+    console.log(error);
+  }
+};
+document.querySelector('.upvote-btn')
+.addEventListener('click', upvoteClickHandler);
