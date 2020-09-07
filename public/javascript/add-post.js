c24939a155ff3751a1aae181a3791ce0987073fc
DJ Viking <anders.swedishviking@gmail.com>
2020-09-06 20:49:40 -0400

    added DOM error feedback for user on add-post page

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,3 +1,4 @@
+const postErrEl = document.querySelector('#post-err');
 const newFormHandler = async (event) => {
   try {
     event.preventDefault();
@@ -5,7 +6,10 @@ const newFormHandler = async (event) => {
     const title = document.querySelector('input[name="post-title"]').value.trim();
     const post_url = document.querySelector('input[name="post-url"]').value.trim();
     //console.log(title, post_url);
-    if (!title || !post_url) throw new Error("Input fields can't be blank.");
+    if (!title || !post_url) {
+      postErrEl.classList.remove('hide-before-error');
+      postErrEl.classList.add('show-after-error');
+    }
     const response = await fetch('/api/posts', {
       method: 'POST',
       body: JSON.stringify(
@@ -23,6 +27,10 @@ const newFormHandler = async (event) => {
     }
     console.log("There was an error.");
     console.log(response.statusText);
+    setTimeout(() => {
+      postErrEl.classList.remove('show-after-error');
+      postErrEl.classList.add('hide-before-error');
+    }, 3000);
   } catch (error) {
     console.log(error);
   }
