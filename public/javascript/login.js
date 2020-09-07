79b252fb9625abbcf12beae3a959629fca7631da
DJ Viking <anders.swedishviking@gmail.com>
2020-09-06 20:42:32 -0400

    display errors on the DOM for the user

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,11 +1,14 @@
+const signupErrEl = document.querySelector('#signup-err');
 const signupFormHandler = async (event) => {
   try {
     event.preventDefault();
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    if (!username || !email || !password) throw new Error("Can't submit that in the form as credentials");
-    //console.log(username, email, password);
+    if (!username || !email || !password) {
+      signupErrEl.classList.remove('hide-before-error');
+      signupErrEl.classList.add('show-after-error');
+    }
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify(
@@ -19,7 +22,7 @@ const signupFormHandler = async (event) => {
         'Content-Type': 'application/json'
       }
     });
-    response.ok ? document.location.reload() : console.log("There was an error."); console.log(response.statusText);
+    response.ok ? document.location.reload() : console.log("There was an error."); console.log(response.statusText); console.log(response.statusText); setTimeout(function(){signupErrEl.classList.remove('show-after-error');signupErrEl.classList.add('hide-before-error');}, 3000);
   } catch (error) {
     console.log(error);
   }
@@ -27,12 +30,16 @@ const signupFormHandler = async (event) => {
 document.querySelector('.signup-form')
 .addEventListener('submit', signupFormHandler);
 
+const loginErrEl = document.querySelector('#login-err');
 const loginFormHandler = async (event) => {
   try {
     event.preventDefault();
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    if (!email || !password) throw new Error("Can't leave those fields blank.");
+    if (!email || !password) {
+        loginErrEl.classList.remove('hide-before-error');
+        loginErrEl.classList.add('show-after-error');
+    }
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify(
@@ -43,7 +50,7 @@ const loginFormHandler = async (event) => {
       ),
       headers: {'Content-Type': 'application/json'}
     });
-    response.ok ? document.location.replace('/') : console.log("There was an error."); console.log(response.statusText);
+    response.ok ? document.location.replace('/') : console.log("There was an error."); console.log(response.statusText); setTimeout(function(){loginErrEl.classList.remove('show-after-error');loginErrEl.classList.add('hide-before-error');},3000);
   } catch (error) {
     console.log(error);
   }
